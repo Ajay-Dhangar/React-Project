fbf70a198dee30a91032eaa3d8798f4dc8af25e5
Ajay Dhangar <99037494+Ajay-Dhangar@users.noreply.github.com>
2022-10-11 23:58:18 +0530

    create react app

---
+++ connect4/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import GameBoard from './component';
+
+
+ReactDOM.render(
+
+    <
+    GameBoard / > ,
+    document.getElementById('root')
+);
